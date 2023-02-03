f32b1bbbe795e23e4c5511dbae2640d342b01fb2
calimad <calimanescuadrian@gmail.com>
2023-02-04 00:10:37 +0200

    install and use PyQt6. Initial button

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import os
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
 
-def shutdown(time):
-    return os.system(f"shutdown -s -t {time}")
 
-shutdown(3600)
+def shutdown():
+    return os.system(f"shutdown -s -t 3600")
 
+
+app = QApplication([])
+
+window = QWidget()
+window.setWindowTitle("Shutdown App")
+layout = QVBoxLayout()
+
+button = QPushButton("Shutdown")
+button.clicked.connect(shutdown)
+
+layout.addWidget(button)
+msgLabel = QLabel("")
+layout.addWidget(msgLabel)
+window.setLayout(layout)
+
+window.show()
+sys.exit(app.exec())
