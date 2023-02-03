df9e71255afcd550029e0d0739efd692cd42a787
calimad <calimanescuadrian@gmail.com>
2023-02-04 01:06:08 +0200

    First shot

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import os
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 
 
-def shutdown():
+def shutdown_1800():
+    return os.system(f"shutdown -s -t 1800")
+
+
+def shutdown_3600():
     return os.system(f"shutdown -s -t 3600")
 
 
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Shutdown App")
+
+        box = QVBoxLayout()
+
+        button1 = QPushButton("30 min")
+        button1.clicked.connect(shutdown_1800)
+        button2 = QPushButton("1 hour")
+        button2.clicked.connect(shutdown_3600)
+
+        box.addWidget(button1)
+        box.addWidget(button2)
+
+        self.setLayout(box)
+
+
 app = QApplication([])
-
-window = QWidget()
-window.setWindowTitle("Shutdown App")
-layout = QVBoxLayout()
-
-button = QPushButton("Shutdown")
-button.clicked.connect(shutdown)
-
-layout.addWidget(button)
-msgLabel = QLabel("")
-layout.addWidget(msgLabel)
-window.setLayout(layout)
-
+window = Window()
 window.show()
 sys.exit(app.exec())
